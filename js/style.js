a28a3a6e9dd98e12d9a0ce1a35548ec43fec58ce
Andrew Parks <parksdrew01@gmail.com>
2017-07-14 08:53:55 -0700

    fix indentation, add if else statement

--- js/style.js
+++ js/style.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
   $("form").submit(function (event){
     event.preventDefault();
-//;
-  //  var question1 = $("#question1").val(); for the text box
+
   var question1 = $("input:radio[name=question1]:checked").val();
   var question2 = $("input:radio[name=question2]:checked").val();
   var question3 = $("input:radio[name=question3]:checked").val();
@@ -12,10 +11,11 @@ $(document).ready(function(){
 
     if(question1 === "seattle" && question2 ===  "math" && question3 === "basketball" && question4 === "pho" && question5 == "yes"){
 
-      $("#output h1").text("Your Match is the University Of Washington!")
+      $("#output h1").text("Your match is the University Of Washington!")
     }
-    else {
-      $("#output h1").text("Your Match is the University Of Oregon!");
+    else if(question1 === "oregon" && question2 ===  "english" && question3 === "soccer" && question4 === "fish" && question5 == "no") {
+
+      $("#output h1").text("Your match is the University Of Oregon!");
     }
 
   });
